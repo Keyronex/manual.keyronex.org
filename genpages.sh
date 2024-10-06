5d03fafadf90343c23f0c7acaaf47d1f4fdd9595
NetBSD User <netbsduser@users.noreply.github.com>
2024-10-06 15:05:20 +0100

    Further pages

--- genpages.sh
+++ genpages.sh
@@ -21,13 +21,28 @@ for SECTION in "${SECTIONS[@]}"; do
             OUTPUT_SECTION_DIR="$OUTPUT_DIR"
             mkdir -p "$OUTPUT_SECTION_DIR"
 
-            # Generate the HTML file
             OUTPUT_HTML="$OUTPUT_SECTION_DIR/$MANPAGE_NAME.$SECTION.html"
 
             echo "Generating HTML for $MANPAGE into $OUTPUT_HTML"
 
-            # Convert man page to HTML using mandoc
-            mandoc -T html -O toc -O man=%N.%S.html   "$MANPAGE" > "$OUTPUT_HTML"
+            cat <<EOF  > "$OUTPUT_HTML"
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <link rel="stylesheet" href="../mandoc.css" type="text/css" media="all"/>
+  <title>${MANPAGE_NAME}.${SECTION}</title>
+</head>
+<body>
+  <div class="heading">
+  <img src="https://github.com/Keyronex/Keyronex/raw/refs/heads/master/docs/keyronex.svg" alt="Keyronex" style="height: 38px; margin-right: 10px;">
+  <span>Keyronex manual pages</span>
+  </div>
+  <br>
+EOF
+            mandoc -T html -O toc,man=%N.%S.html,fragment   "$MANPAGE" >> "$OUTPUT_HTML"
+            echo "</body></html>" >> "$OUTPUT_HTML"
         done
     else
         echo "Man section directory $MAN_SECTION_DIR does not exist. Skipping."
